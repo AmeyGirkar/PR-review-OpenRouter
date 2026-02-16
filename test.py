bbe586c34d253a15202d32236764ce31294fbbdf
AmeyGirkar <amey.girkar.xie@gmail.com>
2026-02-16 12:51:29 +0530

    test

---
+++ test.py
@@ -0,0 +1,14 @@
+def fizz_buzz(n):
+    for i in range(1, n):
+        if i % 15 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz") 
+        elif i % 5 == 0:
+            print("Buzz")
+
+# Security issue
+def add_user(name, password):
+    users = []
+    user = {"name": name, "password": password}  # Plaintext password
+    users.append(user)
